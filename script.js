9e26244c6e37b37419fda7abb3f9bebf3d1738bb
Abdirahman Ali <abdirahmanali@Abdirahmans-MacBook-Pro-2.local>
2023-10-29 03:33:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// ==> STRICT MODE
+// is a special mode that we can activate in Javascript to write a secure code
+"use strict"; // This is how we write it. It has to be the very 1st statement in the script.
+// This makes it easier for us developers accidental errors. Meaning it helps us avoid bugs into our code.
+// First it forbids us to do certain things. Second it create visible errors for us
